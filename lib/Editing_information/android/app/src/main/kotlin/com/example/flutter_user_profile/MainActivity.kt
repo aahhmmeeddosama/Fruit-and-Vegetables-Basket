615bd2579cd63ad8feed506f8a84ac9907dcb642
Marian kromel <71232672+Marian-star-bit@users.noreply.github.com>
2022-01-23 13:45:00 +0200

    Delete lib/Editing_information directory

--- lib/Editing_information/android/app/src/main/kotlin/com/example/flutter_user_profile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_user_profile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/Editing_information/android/app/src/main/kotlin/com/example/yoooh/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.yoooh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
